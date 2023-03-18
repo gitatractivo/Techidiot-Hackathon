c9b0538fd960461c703b288fc90effca13a79758
Samarth Bagga <samarthbagga@gmail.com>
2023-03-18 19:01:50 +0530

    nano changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Hero from "./pages/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
 import Reviews from "./pages/Reviews";
-import Login from "./pages/Login";
-import Signup from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/review" element={<Reviews />} />
       </Routes>
