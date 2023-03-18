c3d228943b2a7aec7dcfc68bc823f0536a664ec7
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-18 13:34:58 +0530

    NavBar Added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import MyApp from "./components/MyApp";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    <MyApp></MyApp>
-    </div>
+      <Navbar></Navbar>
+    </div>  
   );
 }
 
