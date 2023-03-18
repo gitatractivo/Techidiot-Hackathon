fe9fb4f0d4589a87c3b59bcac25de6677a4cf1bd
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-18 19:55:19 +0530

    SignupVersion2.0

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,79 +1,89 @@
 import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  Button,
+} from "@mui/material";
 
-const Signup = () => {
-  const [formData, setFormData] = useState({});
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
+    const { id, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(credentials);
   };
 
   return (
-    <div className="w-5/6 mx-auto h-full">
-      <div className="flex justify-center items-center h-full">
-        <div className="bg-white">
-          <form
-            onSubmit={(e) => handleSubmit(e)}
-            className="flex flex-col gap-10 p-10"
-          >
-            <h2 className="text-center font-serif text-pink-500">
-              SIGN~UP
-            </h2>
-            <p className="my-1">
-              <input
-                type="text"
-                placeholder="ENTER FIRSTNAME"
-                required
-                name="firstName"
-                onChange={handleChange}
-                className="px-6 py-3 border outline-none border-cyan-500 focus:border-pink-400"
-              />
-            </p>
-            <p className="my-1">
-              <input
-                type="text"
-                placeholder="ENTER LASTNAME"
-                required
-                name="lastName"
-                onChange={handleChange}
-                className="px-6 py-3 border outline-none border-cyan-500 focus:border-pink-400"
-              />
-            </p>
-            <p className="my-1">
-              <input
-                type="email"
-                placeholder="ENTER EMAIL"
-                name="email"
-                required
-                onChange={(e) => handleChange(e)}
-                className="px-6 py-3 outline-none border border-cyan-500 focus:border-pink-400"
-              />
-            </p>
-            <p className="my-1">
-              <input
-                type="password"
-                placeholder="ENTER PASSWORD"
-                required
-                name="password"
-                onChange={handleChange}
-                className="px-6 py-3 border outline-none border-cyan-500 focus:border-pink-400"
-              />
-            </p>
-            <button className="py-2 px-4 border rounded-xl bg-pink-400 w-full text-center text-white font-bold hover:bg-pink-300 hover:text-black">
-              Submit
-            </button>
-          </form>
-          {/* for social media */}
+    <div className="w-5/6 mx-auto flex flex-col gap-10 md:flex-row md:justify-between mt-10">
+      <div className="flex flex-col basis-1/2 gap-10 items-center justify-center ">
+        <h1 className="text-4xl">
+          "I will love the light for it shows me the way, yet I will endure the
+          darkness because it shows me the stars."
+        </h1>
+        <p>Og Mandino</p>
+      </div>
+      <div className="basis-1/2">
+        <div className="flex flex-col gap-12 px-20 py-20">
+          <FormControl>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">
+              We'll never share your email.
+            </FormHelperText>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel htmlFor="my-password">Password</InputLabel>
+            <Input id="my-password" aria-describedby="my-password-text" />
+            <FormHelperText id="my-password-text">
+              We'll never share your password.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor="first-name-input">First Name</InputLabel>
+            <Input
+              id="first-name-input"
+              aria-describedby="first-name-helper-text"
+              onChange={handleChange}
+            />
+            <FormHelperText id="first-name-helper-text">
+              Please enter your first name.
+            </FormHelperText>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel htmlFor="last-name-input">Last Name</InputLabel>
+            <Input
+              id="last-name-input"
+              aria-describedby="last-name-helper-text"
+              onChange={handleChange}
+            />
+            <FormHelperText id="last-name-helper-text">
+              Please enter your last name.
+            </FormHelperText>
+          </FormControl>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Login
+          </Button>
         </div>
       </div>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
