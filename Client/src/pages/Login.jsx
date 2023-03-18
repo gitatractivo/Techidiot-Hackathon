5aacf407920394333ea2884a06860f452d07a66e
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-18 19:51:48 +0530

    LOGIN PAGE v2.0

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FormControl, InputLabel, Input, FormHelperText,Button } from "@mui/material";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -21,25 +22,34 @@ const Login = () => {
 
   return (
     <div className="w-5/6 mx-auto flex flex-col gap-10 md:flex-row md:justify-between mt-10">
-      <div className="flex flex-col">
-        <h1></h1>
-        <p></p>
+      <div className="flex flex-col basis-1/2 gap-10 items-center justify-center ">
+        <h1 className="text-4xl">
+          "I will love the light for it shows me the way, yet I will endure the
+          darkness because it shows me the stars."
+        </h1>
+        <p>Og Mandino</p>
       </div>
-      <div >
-        <form className="form" onSubmit={handleSubmit}>
-          <p>
-            <label htmlFor="name"></label>
-          </p>
-          <input type="text" name="" placeholder="ENTER YOUR NAME" />
-          <p>
-            <label htmlFor="name"></label>
-          </p>
-          <input
-            type="text"
-            name="password"
-            placeholder="ENTER YOUR PASSWORD"
-          />
-        </form>
+      <div className="basis-1/2">
+        <div className="flex flex-col gap-24 px-20 py-20">
+          <FormControl>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">
+              We'll never share your email.
+            </FormHelperText>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel htmlFor="my-password">Password</InputLabel>
+            <Input id="my-password" aria-describedby="my-password-text" />
+            <FormHelperText id="my-password-text">
+              We'll never share your password.
+            </FormHelperText>
+          </FormControl>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Login
+          </Button>
+        </div>
       </div>
     </div>
   );
