c4fc2905e16d6dcc07889564b88748fe9bd1fff3
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-18 14:26:58 +0530

    Basic Landing Page ready v-1.0

---
+++ Client/src/pages/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import MediaControlCard from "../components/CardComponent";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="w-5/6 mx-auto py-56 px-20">
+        <div className="flex flex-col gap-12">
+          <div>
+            <h1 className="font-bold tracking-wide text-[2rem]">
+              You deserve to be happy.
+            </h1>
+            <p>What type of therapy are you looking for?</p>
+          </div>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3 md:gap-10">
+            <div>
+              <MediaControlCard></MediaControlCard>
+            </div>
+            <div>
+              <MediaControlCard></MediaControlCard>
+            </div>
+            <div>
+              <MediaControlCard></MediaControlCard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
