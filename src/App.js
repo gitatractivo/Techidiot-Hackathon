4b6b7005c321d65a44da160ce9a54b084c83d2ef
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-18 12:03:04 +0530

    everything installed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyApp from "./components/MyApp";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <MyApp></MyApp>
+    </div>
+  );
+}
+
+export default App;
